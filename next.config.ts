6f96c79f2eeaa408bbdeb44fd614cd00005e41b2
atlasboran <akyakapro@gmail.com>
2026-07-29 20:27:40 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+  import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.106", "localhost", "127.0.0.1"],
-};
+  const nextConfig: NextConfig = {
+    output: "export",
+    allowedDevOrigins: ["192.168.1.106", "localhost", "127.0.0.1"],
+  };
 
-export default nextConfig;
+  export default nextConfig;
