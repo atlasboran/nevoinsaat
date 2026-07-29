ab8850d9e1d829222cc2b525d316e4afd762fd5a
atlasboran <akyakapro@gmail.com>
2026-07-29 20:52:04 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@
 
   const nextConfig: NextConfig = {
     output: "export",
+      images: {
+    unoptimized: true,
+  },
+
+  Başlangıç şöyle görünmeli:
+
+  const nextConfig: NextConfig = {
+    output: "export",
+    images: {
+      unoptimized: true,
+    },
+    allowedDevOrigins: ["192.168.1.106", "localhost", "127.0.0.1"],
+  };
     allowedDevOrigins: ["192.168.1.106", "localhost", "127.0.0.1"],
   };
 
